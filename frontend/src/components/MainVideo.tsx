3a5ae66fd0c27859751dc9e71843ece9e6926dfe
FLIPPED-RN <rnazarov142@gmail.com>
2025-09-29 16:30:31 +0200

    create a video component

--- frontend/src/components/MainVideo.tsx
+++ frontend/src/components/MainVideo.tsx
@@ -1,7 +1,10 @@
+import Video from "../assets/video.jpg"
+
 export default function MainVideo(){
     return(
-        <div>
-            
+        <div className="main-video__block">
+            <h2>Watch video to discover advantages</h2>
+            <img src={Video} alt="" />
         </div>
     )
 }
